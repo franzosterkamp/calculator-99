8f44a424e61d00e78be618e86b9bf42b6b140119
franzosterkamp <franz.osterkamp@gmx.net>
2019-11-06 13:48:49 +0100

    add operators in functions

--- src/index.js
+++ src/index.js
@@ -1,26 +1,42 @@
+"use strict";
+
+import { sum, minus, multi, divide } from "./lib/operators";
+
 const firstElement = document.querySelector("[name=first]");
 const secondElement = document.querySelector("[name=second]");
 const plusButton = document.querySelector(".plus");
 const minusButton = document.querySelector(".minus");
 const multiButton = document.querySelector(".multi");
 const divideButton = document.querySelector(".divide");
+const operator = document.querySelector(".operator");
+// const result = document.getElementById("result");
+const result = document.querySelector("#result");
 
 plusButton.addEventListener("click", function() {
-  const sum = parseInt(firstElement.value) + parseInt(secondElement.value);
-  alert(sum);
+  // const sum = parseInt(firstElement.value) + parseInt(secondElement.value);
+  const firstNumber = parseInt(firstElement.value);
+  const secondNumber = parseInt(secondElement.value);
+  result.innerHTML = sum(firstNumber, secondNumber);
+  operator.innerHTML = "+";
 });
 
 minusButton.addEventListener("click", function() {
-  const sum = parseInt(firstElement.value) - parseInt(secondElement.value);
-  alert(sum);
+  const firstNumber = parseInt(firstElement.value);
+  const secondNumber = parseInt(secondElement.value);
+  result.innerHTML = minus(firstNumber, secondNumber);
+  operator.innerHTML = "-";
 });
 
 multiButton.addEventListener("click", function() {
-  const sum = parseInt(firstElement.value) * parseInt(secondElement.value);
-  alert(sum);
+  const firstNumber = parseInt(firstElement.value);
+  const secondNumber = parseInt(secondElement.value);
+  result.innerHTML = multi(firstNumber, secondNumber);
+  operator.innerHTML = "x";
 });
 
 divideButton.addEventListener("click", function() {
-  const sum = parseInt(firstElement.value) / parseInt(secondElement.value);
-  alert(sum);
+  const firstNumber = parseInt(firstElement.value);
+  const secondNumber = parseInt(secondElement.value);
+  result.innerHTML = divide(firstNumber, secondNumber);
+  operator.innerHTML = "/";
 });
---
+++ src/lib/operators.js
@@ -0,0 +1,15 @@
+export function sum(first, second) {
+  return first + second;
+}
+
+export function minus(first, second) {
+  return first - second;
+}
+
+export function multi(first, second) {
+  return first * second;
+}
+
+export function divide(first, second) {
+  return first / second;
+}
