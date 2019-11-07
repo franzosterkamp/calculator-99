a0a7ca7d355b7ea4a196cde40f074915fca0ebc1
Franz Osterkamp <56834039+franzosterkamp@users.noreply.github.com>
2019-11-07 13:44:17 +0100

    Refactor function and some Style (#10)
    * Refactor function and some Style
    
    * Tobi hat mich gezwungen

--- src/lib/elements.js
+++ src/lib/elements.js
@@ -1,6 +1,13 @@
+import { countDigits } from "./operators";
+
+const operator = document.querySelector(".operator");
+const result = document.querySelector("#result");
+const digitCounter = document.querySelector(".digitcounter");
+const firstElement = document.querySelector("[name=first]");
+const secondElement = document.querySelector("[name=second]");
 export function createDivWithContent(content) {
   content = content.toString();
-  let divElement = document.createElement("div");
+  const divElement = document.createElement("div");
   divElement.innerHTML = content;
 
   return divElement;
@@ -8,4 +15,16 @@ export function createDivWithContent(content) {
 
 export function appendElementToElement(parent, child) {
   return parent.appendChild(child);
+}
+
+export function handleClick(operatorSign, operatorFunction) {
+  const firstNumber = parseInt(firstElement.value);
+  const secondNumber = parseInt(secondElement.value);
+  let childResult = operatorFunction(firstNumber, secondNumber);
+  childResult = createDivWithContent(childResult);
+  appendElementToElement(result, childResult);
+  operator.innerHTML = operatorSign;
+  digitCounter.innerHTML = countDigits(
+    operatorFunction(firstNumber, secondNumber)
+  );
 }
