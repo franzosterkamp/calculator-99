37b6e266f64a90e3655b53ef49f7a2a2fed6d8d9
Franz Osterkamp <56834039+franzosterkamp@users.noreply.github.com>
2019-11-07 14:37:27 +0100

    finish Style and one more Result Line (#12)

--- src/lib/elements.js
+++ src/lib/elements.js
@@ -3,6 +3,7 @@ import { countDigits } from "./operators";
 const operator = document.querySelector(".operator");
 const result = document.querySelector("#result");
 const digitCounter = document.querySelector(".digitcounter");
+let resultFormel = document.querySelector(".result-formel");
 const firstElement = document.querySelector("[name=first]");
 const secondElement = document.querySelector("[name=second]");
 export function createDivWithContent(content) {
@@ -21,9 +22,12 @@ export function handleClick(operatorSign, operatorFunction) {
   const firstNumber = parseInt(firstElement.value);
   const secondNumber = parseInt(secondElement.value);
   let childResult = operatorFunction(firstNumber, secondNumber);
+  let childResultFormel = `${firstNumber} ${operatorSign} ${secondNumber} = `;
+  childResultFormel = createDivWithContent(childResultFormel);
   childResult = createDivWithContent(childResult);
   appendElementToElement(result, childResult);
   operator.innerHTML = operatorSign;
+  appendElementToElement(resultFormel, childResultFormel);
   digitCounter.innerHTML = countDigits(
     operatorFunction(firstNumber, secondNumber)
   );
